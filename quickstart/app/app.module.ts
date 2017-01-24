fe1e51e6ebfe63301e4ae27315a1326ef3f3f76b
btd1337 <helder.bertoldo@ice.ufjf.br>
2017-01-24 17:58:45 -0200

    Initial commit

---
+++ quickstart/app/app.module.ts
@@ -0,0 +1,14 @@
+import { AppComponent }  from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule }      from '@angular/core';
+
+@NgModule({
+  imports: [ 
+    BrowserModule,
+    FormsModule
+   ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
